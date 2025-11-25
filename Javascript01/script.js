2cb9e5b3dd75e06eeba2094854064ad3004e81dc
Richard Suarez <richard.suarezjacome2021@gmail.com>
2025-11-24 19:04:08 -0500

    22 de Noviembre 2025

---
+++ Javascript01/script.js
@@ -0,0 +1,56 @@
+console.log("Hola mundo");
+
+let nombre = "Julian Hoyos";
+let edad = 15;
+let boleano = true;
+
+nombre = 15;
+console.log(typeof nombre)
+
+let persona = {
+  nombre : "Juanita",
+  apellido : "Alimaña",
+  edad: 25,
+  soltera: true
+}
+console.log(typeof persona.soltera);
+
+function miPrimeraFuncion(){
+    return "Hola Mundo en Javascript";
+}
+
+console.log(typeof miPrimeraFuncion());
+
+let x;
+
+console.log(typeof x);
+
+let a=null;
+console.log(typeof a);
+
+
+let apellidos = "Ramirez Ordoñez";
+let edadNuevo = 30;
+
+console.log(`Hola Mis apellidos son ${apellidos} y mi edad es ${edadNuevo}`)
+
+let b=2;
+let c =3;
+let suma = b + c;
+console.log(suma);
+
+
+var z = 100;
+var z = 10
+console.log(z);
+
+let y = 15;
+///let y =100;
+y = 20;
+console.log(y);
+
+
+const PI = 3.14159; // const PI = 3.14; //Error: no se puede redeclarar // PI = 3.14; //Error: no se puede cambiar console.log(PI); // 3.14159
+
+
+
